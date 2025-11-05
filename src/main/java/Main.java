f229061dc53c1ebbe07d19d47fca9c8ba229e386
Patman1O1 <patrickthomas101x@gmail.com>
2025-11-05 11:15:23 -0600

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    /* --------------------------------------------------Fields------------------------------------------------------ */
+    /* --------------------------------------------------Methods----------------------------------------------------- */
+    @Override
+    public void start(Stage stage) {
+
+    }
+
+}
